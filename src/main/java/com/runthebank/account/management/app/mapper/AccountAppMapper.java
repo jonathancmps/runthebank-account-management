9ad41b5faea6254eec831fd92739c3949b6c7080
Jonathan Campos <jonathan.campos.net@gmail.com>
2025-01-15 01:32:20 -0300

    refactor(account): melhorias no fluxo de cadastro de contas

--- src/main/java/com/runthebank/account/management/app/mapper/AccountAppMapper.java
+++ src/main/java/com/runthebank/account/management/app/mapper/AccountAppMapper.java
@@ -1,10 +1,22 @@
 package com.runthebank.account.management.app.mapper;
 
 import com.runthebank.account.management.app.dto.AccountDTO;
+import com.runthebank.account.management.domain.enums.AccountStatus;
 import com.runthebank.account.management.domain.model.Account;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface AccountAppMapper {
+
+    @Mapping(target = "status", source = "status", qualifiedByName = "setDefaultStatus")
     Account toModel(AccountDTO accountDto);
+
+    @Named("setDefaultStatus")
+    default AccountStatus setDefaultStatus(AccountStatus status) {
+        return status != null ? status : AccountStatus.ACTIVE;
+    }
+
+
 }
--- src/main/java/com/runthebank/account/management/domain/usecase/AccountUseCase.java
+++ src/main/java/com/runthebank/account/management/domain/usecase/AccountUseCase.java
@@ -1,23 +1,37 @@
 package com.runthebank.account.management.domain.usecase;
 
+import com.runthebank.account.management.domain.exception.CustomerNotPresentException;
 import com.runthebank.account.management.domain.exception.DuplicatedResourceException;
 import com.runthebank.account.management.domain.interfaces.AccountProvider;
+import com.runthebank.account.management.domain.interfaces.CustomerProvider;
 import com.runthebank.account.management.domain.model.Account;
 
 public class AccountUseCase {
 
+    private CustomerProvider customerProvider;
     private AccountProvider accountProvider;
 
-    public AccountUseCase(AccountProvider accountProvider) {
+    public AccountUseCase(CustomerProvider customerProvider, AccountProvider accountProvider) {
+        this.customerProvider = customerProvider;
         this.accountProvider = accountProvider;
     }
 
     public Account createAccount(Account account) {
-        var  existingAgency = findExistingAgency(account.getBranchCode());
+        var existingAgency = findExistingAgency(account.getBranchCode());
+        var existingDocumentNumber = customerProvider.findCustomerByDocumentNumber(account.getDocumentNumber());
+
         if(existingAgency) {
             throw new DuplicatedResourceException(String.format("agency %s already registered", account.getBranchCode()));
         }
+
+        if(!existingDocumentNumber) {
+            throw new CustomerNotPresentException(String.format("documentNumber %s is not registered", account.getBranchCode()));
+        }
         return accountProvider.createAccount(account);
+    }
+
+    public Account saveAccount(Account account) {
+        return accountProvider.saveAccount(account);
     }
 
     public boolean findExistingAgency(String branchCode) {
